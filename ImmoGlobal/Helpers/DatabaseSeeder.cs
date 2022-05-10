c7e37e4ccf7f2f28e0ae047079a6f05b87013bfb
sirtheta <neo38@bluewin.ch>
2022-05-10 16:53:21 +0200

    db seeder changes

--- ImmoGlobal/Helpers/DatabaseSeeder.cs
+++ ImmoGlobal/Helpers/DatabaseSeeder.cs
@@ -14,12 +14,12 @@ namespace ImmoGlobal.Helpers
     {
 
       var _renter = new Persona("Musterman", "Max", 0334328978, "mail@mail.de", DateTime.Parse("25.10.1988"), "Barstreet 5", 3612, "Steffisburg", ECivilState.Married, "Thunstr. 15, 3600 Bern", "12ET34UI56789", 0791234567);
-      var _creditor = new Persona(true, "Creditors Company Name", "Creditors Company Address", 3800, "Interlaken", 0797891236, "Creditor ContactPerson", "CreditorName", "CreditorSurname", "mail@test.de", "CHE-123.456.789");
+      var _creditor = new Persona(true, "Creditors Company Name", "Creditors Company Address", 3800, "Interlaken", 0797891236, "Creditor ContactPerson", "CreditorFirstName", "CreditorLastName", "mail@test.de", "CHE-123.456.789");
       var _housekeeper = new Persona("Houskeeperman", "Moritz", 0238905678, "moritz@mail.de", "Obsolet Adress 4", 2344, "Ronmalingen", "893489uip099322", 8907894561);
       var _housekeeper2 = new Persona("Schneider", "Peter", 0236565678, "peter@schneider.de", "Obsolet Adress 5", 3262, "Tübingen", "893489uip6544542", 5896314782);
 
-      var _property1 = new Property() { Housekeeper = _housekeeper, Description = "TestLiegenschaft1", Address = "TestAdress1", ZipCode = 3612, City = "TestCity1", PropertyInsurance = "Helvetia1", PersonInsurance = "Mobiliar1", LiabilityInsurance = "Emmitaler1" };
-      var _property2 = new Property() { Housekeeper = _housekeeper2, Description = "TestLiegenschaft2", Address = "TestAdress2", ZipCode = 3613, City = "TestCity2", PropertyInsurance = "Helvetia2", PersonInsurance = "Mobiliar2", LiabilityInsurance = "Emmitaler2" };
+      var _property1 = new Property() { Housekeeper = _housekeeper, Description = "Wohngemeinschaft Au", Address = "TestAdress1", ZipCode = 3612, City = "TestCity1", PropertyInsurance = "Helvetia1", PersonInsurance = "Mobiliar1", LiabilityInsurance = "Emmitaler1" };
+      var _property2 = new Property() { Housekeeper = _housekeeper2, Description = "Westquartier", Address = "TestAdress2", ZipCode = 3613, City = "TestCity2", PropertyInsurance = "Helvetia2", PersonInsurance = "Mobiliar2", LiabilityInsurance = "Emmitaler2" };
 
       var _objectHouse = new PropertyObject() { Property = _property1, ObjectType = EPropertyObjectType.House, Description = "Wohnung 1", Location = "TestLocation", NumberOfRooms = 4.5, Area = 100, NumberOfKeys = 5, Fridge = true, Dishwasher = true, Stove = true, Oven = true };
       var _objectGarage = new PropertyObject() { Property = _property1, ObjectType = EPropertyObjectType.Garage, Description = "Garage von Wohnung 1", Location = "irgendwo nebenan", Area = 15 };
@@ -164,11 +164,11 @@ namespace ImmoGlobal.Helpers
       var _user2 = new User() { FirstName = "Patrick", LastName = "Graber", Password = SecurePasswordHasher.Hash("2"), Email = "2", Role = ERole.User };
       var _user3 = new User() { FirstName = "Test", LastName = "User", Password = SecurePasswordHasher.Hash("3"), Email = "3", Role = ERole.Viewer };
 
-
+      
       List<Property> properties = new();
       for (int i = 0; i < 20; i++)
       {
-        properties.Add(new Property() { Housekeeper = _housekeeper, Description = "TestLiegenschaft1", Address = "TestAdress1", ZipCode = 3612, City = "TestCity1", PropertyInsurance = "Helvetia1", PersonInsurance = "Mobiliar1", LiabilityInsurance = "Emmitaler1" });
+        properties.Add(new Property() { Housekeeper = _housekeeper, Description = $"TestLiegenschaft {i}", Address = "TestAdress1", ZipCode = 3612, City = "TestCity1", PropertyInsurance = "Helvetia1", PersonInsurance = "Mobiliar1", LiabilityInsurance = "Emmitaler1" });
       }
 
 
